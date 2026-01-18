c423dcc1834226612578a6b1c28b8c8a5fe0bc47
Pulkit <pulkit@scanbrix.com>
2026-01-19 02:12:37 +0530

    Update __init__.py

--- sketchup_importer/slapi/__init__.py
+++ sketchup_importer/slapi/__init__.py
@@ -1,3 +1,5 @@
 __author__ = 'Martijn Berger'
 
 __all__ = ['geometry','slapi','entities','camera','model','transformation']
+from . import * ```
+*(Or whatever code is needed to expose the SketchUp C-functions you uploaded).*
